6fb119fe2c30799dfff7320e296eb393c0ec31fd
terunobu3 <165560137+terunobu3@users.noreply.github.com>
2024-07-14 04:48:11 +0900

    プルリクエスト時訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :comments
-  has_many :orders
+  #has_many :items
+  #has_many :comments
+  #has_many :orders
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'には全角カタカナを使用してください' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'には全角カタカナを使用してください' }
     validates :birthday
