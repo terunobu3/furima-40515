9d44754298a2900d86ffe6f11e8d695a1e192f13
terunobu3 <165560137+terunobu3@users.noreply.github.com>
2024-07-26 09:54:15 +0900

    税自動入力の訂正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
