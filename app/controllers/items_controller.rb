5f7f2e37ccce0d1d8b7615354739bc7656e3ee57
terunobu3 <165560137+terunobu3@users.noreply.github.com>
2024-08-03 15:17:26 +0900

    commit 9-1
    destroyアクションのルーティング
    削除ボタンの実装
    destroyアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: :DESC)
@@ -31,6 +31,13 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
     end
   end
 
