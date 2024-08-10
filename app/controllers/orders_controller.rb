4f9fa23cb613e595b8b9c20991bc63befea58168
terunobu3 <165560137+terunobu3@users.noreply.github.com>
2024-08-10 15:05:23 +0900

    modification PR 10-4
    指摘されたことの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    if user_signed_in? && current_user.id != @item.user_id
+    if user_signed_in? && current_user.id != @item.user_id || @item.order.present?
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @user_order = UserOrder.new
     else
@@ -12,7 +12,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     if @user_order.valid?
       pay_item(@item.price)
       @user_order.save
