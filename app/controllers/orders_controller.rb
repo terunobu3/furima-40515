ddcf7faf2055e235a4e18e1f79f2e9a2b2123980
terunobu3 <165560137+terunobu3@users.noreply.github.com>
2024-08-10 15:48:12 +0900

    modification PR 10-6

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    if user_signed_in? && current_user.id != @item.user_id || @item.order.present?
+    if user_signed_in? && current_user.id != @item.user_id ! @item.order.present?
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @user_order = UserOrder.new
     else
